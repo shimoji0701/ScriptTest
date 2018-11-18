28da4676aad6f344753aa5261f225c47206e1c83
yousuke_shimoji <shimoji.0701@outlook.jp>
2018-11-18 18:39:51 +0900

    ScriptTest  課題提出
    配列を宣言して出力
    Magic関数の呼び出し

---
+++ Assets/Test.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss{
+
+    int mp = 53;
+
+    public void Magic(int use)   {
+        if (mp >= use) {
+            this.mp -= use;
+            Debug.Log("魔法攻撃した。残りｍｐは" + mp);
+
+        } else {
+            Debug.Log("mpが足りないため魔法が使えません。");
+        }
+    }
+}
+
+public class Test : MonoBehaviour {
+    // Use this for initialization
+    void Start()
+    {
+        int[] array = { 10, 20, 30, 40, 50 };
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        //int[] arrays = new int[5];
+
+        // arrays[0] = 10;
+        // arrays[1] = 20;
+        //arrays[2] = 30;
+        // arrays[3] = 40;
+        // arrays[4] = 50;
+
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+        Boss lastboss = new Boss();
+
+        for (int i = 0; i < 11; i++) {
+            lastboss.Magic(5);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+}  
+    
+
